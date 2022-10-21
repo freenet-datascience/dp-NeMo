177711ea1a6beea3560abcf22ff55039ac85c728
Magnus Halbe <magnus.halbe@web.de>
2022-10-21 07:41:07 +0000

    useable docker file

---
+++ mountable/my_converter/soundsLikeReplacer.py
@@ -0,0 +1,30 @@
+import csv
+import fileinput
+import sys
+
+
+fileToReplaceIn = sys.argv[1]
+changeToSoundsLike = sys.argv[2] # use 'train' for training data. Words get replaced with their soundslike.
+# use 'result' for conversion from NeMo transcript to useable transcript for use
+if changeToSoundsLike != "train" and changeToSoundsLike != "result":
+    print("Second argument must be 'train' or 'result'!")
+    exit()
+
+with fileinput.FileInput(fileToReplaceIn, inplace=True, backup='.bak') as file:
+    for line in file:
+        with open('/workspace/NeMo/audio/my_converter/custom_words_nemo.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=';')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    if row[0] != row[1]:
+                        firstSoundsLike = row[1].split(',')[0]
+                        if changeToSoundsLike == "train":
+                            line = line.replace(row[0], firstSoundsLike)
+                        elif changeToSoundsLike == "result":
+                            line = line.replace(firstSoundsLike, row[0])
+                        line_count += 1
+            print(line, end='') 
+print("done!")
