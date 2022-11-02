5628b1a491d054f02f55b890dcfd1e9b05302775
Magnus Halbe <magnus.halbe@web.de>
2022-11-02 16:51:10 +0000

    accommodations for parlance use

--- mountable/my_converter/soundsLikeReplacer.py
+++ mountable/my_converter/soundsLikeReplacer.py
@@ -12,7 +12,7 @@ if changeToSoundsLike != "train" and changeToSoundsLike != "result":
 
 with fileinput.FileInput(fileToReplaceIn, inplace=True, backup='.bak') as file:
     for line in file:
-        with open('/workspace/NeMo/audio/my_converter/custom_words_nemo.csv') as csv_file:
+        with open('/workspace/NeMo/mountable/my_converter/custom_words_nemo.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=';')
             line_count = 0
             for row in csv_reader:
